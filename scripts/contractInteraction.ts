51d92cfc24b485ba1d0dae2de24310eeaf301241
Alfa11917676 <alfatitas747ray@outlook.com>
2023-12-17 13:05:21 +0530

    discord ping: test

--- scripts/contractInteraction.ts
+++ scripts/contractInteraction.ts
@@ -17,9 +17,9 @@ async function main () {
     const cca = await CCA.attach("0xB3B4C26f741d726A8121F0bfc2bBeb9C21792a20");
     console.log("The Aditus contract is attached to:", cca.target);
 
-    let tx = await cca.totalSupply();
-    console.log("Deployer address: ", deployer.address);
-    console.log("total supply:", tx.toString());
+    // let tx = await cca.totalSupply();
+    // console.log("Deployer address: ", deployer.address);
+    // console.log("total supply:", tx.toString());
 
     // let burn = await cca.burn(0);
     // await burn.wait();
