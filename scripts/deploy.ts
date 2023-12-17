88118369c2bf02db592be4243a2c3e70dcbdedaf
Alfa11917676 <alfatitas747ray@outlook.com>
2023-12-17 12:27:51 +0530

    deleted duplicate

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,26 @@
-import { ethers } from "hardhat";
+//write script to deploy smart contract
+// Path: scripts/deploy.ts
 
-async function main() {
+import {ethers, upgrades} from "hardhat";
+
+async function main () {
+    const [deployer] = await ethers.getSigners();
+    const CCA = await ethers.getContractFactory("CreepCrewGenesis");
+    const os = "0x3cc6CddA760b79bAfa08dF41ECFA224f810dCeB6"
+    const cca = await upgrades.deployProxy(CCA, [os], {initializer: 'initialize'});
+    await cca.waitForDeployment();
+    console.log("cca deployed to:", cca.target);
+
+    let tx = await cca.addController(deployer.address);
+    await tx.wait();
+    console.log("controller added");
+
+    tx = await cca.mint(deployer.address, 10);
+    await tx.wait();
+    console.log("minted");
+
+
 
 }
-
-async function verify(contractAddress:string, contructorArgs:any[]) {
-  await hre.run("verify:verify", {
-    address: contractAddress,
-    constructorArguments: [contructorArgs],
-  });
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+ main()
+//fda5dd9bbc3dc2d573adf6fffa118cd72db459c4091bc46e5b346246700399d4
