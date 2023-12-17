10168dca03267b0e1d247a2262756b867a92994f
Alfa11917676 <alfatitas747ray@outlook.com>
2023-12-17 14:12:44 +0530

    Update contractInteraction.ts

--- scripts/contractInteraction.ts
+++ scripts/contractInteraction.ts
@@ -14,8 +14,8 @@ async function main () {
 
     const [deployer] = await ethers.getSigners();
     const CCA: CreepCrewAditus__factory = await ethers.getContractFactory("CreepCrewAditus");
-    const cca = await CCA.attach("0xB3B4C26f741d726A8121F0bfc2bBeb9C21792a20");
-    console.log("The Aditus contract is attached to:", cca.target);
+    // const cca = await CCA.attach("0xB3B4C26f741d726A8121F0bfc2bBeb9C21792a20");
+    // console.log("The Aditus contract is attached to:", cca.target);
     //
     // let tx = await cca.totalSupply();
     // console.log("Deployer address: ", deployer.address);
